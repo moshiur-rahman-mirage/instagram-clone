d5ad6a859b4ad1d8ae399fabd742f1f0c273e159
Moshiur Rahman Taz Mirage <moshiur.mirage@gmail.com>
2024-01-17 23:52:27 +0600

    auth page with staic functionality done.

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
-  )
-}
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          {/* Left Hand Sib de */}
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="phone.img" />
+          </Box>
+          {/* Right Hand Side */}
+          <VStack spacing={4} align={"streatch"}>
+            <AuthForm />
+            <Box textAlign={"center"}>Get The App</Box>
+            <Flex gap={5} justifyContent={"center"}>
+              <Image src="/playstore.png" h={10} alt="playstore.logo" />
+              <Image src="/microsoft.png" h={10} alt="Microsoft.logo" />
+            </Flex>
+          </VStack>
+        </Flex>
+      </Container>
+    </Flex>
+  );
+};
 
-export default AuthPage
+export default AuthPage;
